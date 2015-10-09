33849f6ec8ccf2869add1d03acb8e53e6d87388f
Kman Toro <camonbuller@yahoo.com>
2015-10-09 12:38:59 -0500

    trying to get my head on straight.

--- src/Addition_Game.java
+++ src/Addition_Game.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Addition_Game {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		
+	int num1 = math.random();
 	
-		System.out.println ("hello world");
+	System.out.println(num1);
 		
 	}
 
---
+++ src/Camon_TryTo.java
@@ -0,0 +1,12 @@
+
+public class Camon_TryTo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
