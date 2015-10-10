be50a66e286968031da8129bbf7a97aa01bc70f5
Kman Toro <camonbuller@yahoo.com>
2015-10-10 18:58:32 -0500

    adding a random number generator.

--- src/Addition_Game.java
+++ src/Addition_Game.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Addition_Game {
 public static void main(String[] args) {
 		
-	int num1 = math.random();
+
+	
+	int num1 = (int) (Math.random() * 10)+1;
 	
 	System.out.println(num1);
 		
