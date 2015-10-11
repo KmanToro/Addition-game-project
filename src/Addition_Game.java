4b7a2784e8b9aec9e81927095820141f3ea6902b
Kman Toro <camonbuller@yahoo.com>
2015-10-10 22:14:04 -0500

    round one and begining the determinng points.

--- src/Addition_Game.java
+++ src/Addition_Game.java
@@ -1,15 +1,35 @@
 import java.util.Scanner;
-import java.io.*;
 
 public class Addition_Game {
 public static void main(String[] args) {
-		
+	
+//establish point system
+	int points = 0;
+	System.out.println("Your poiunt =" + points);
+	
+//asking the user to add two different numbers
+	System.out.println("What is the sum of the following numbers");
 
+//making initial random numbers
+	int randomnum1 = (int) (Math.random() * 9)+1;
+	int randomnum2 = (int) (Math.random() * 9)+1;
+	int CorrectAnswer1 = (int) randomnum1 + (int)randomnum2;
+	Scanner input = new Scanner(System.in);
 	
-	int num1 = (int) (Math.random() * 10)+1;
+	System.out.println(randomnum1 + " and " + randomnum2 + "=");	
+	int GivenAnswer = input.nextInt();
 	
-	System.out.println(num1);
-		
+	if (GivenAnswer == CorrectAnswer1)
+	{
+	System.out.println("Correct!!");
+	System.out.println("your points = " + points +1);
+	}
+	else
+	{
+	System.out.println("Sorry your answer is incorrect. :( ");
+	System.out.println("your points = " + points +0);	
 	}
 
+
+}
 }
